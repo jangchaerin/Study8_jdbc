dce03d4e4d9059320de8ef2c92e533b7bd7c5547
jangchaerin <cofls3928@naver.com>
2022-01-19 12:38:50 +0900

    d

---
+++ src/com/chaerin/s1/start/StartMain.java
@@ -0,0 +1,10 @@
+package com.chaerin.s1.start;
+
+public class StartMain {
+
+	public static void main(String[] args) {
+		System.out.println("db연동 테스트 시작");
+		System.out.println("db연동 테스트 끝");
+	}
+
+}
